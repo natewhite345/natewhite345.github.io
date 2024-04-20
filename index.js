1ef36550d4dd2bd6c685cefbb8247f01d5e118b4
natetheadequate <natewhite345@gmail.com>
2024-04-20 00:26:05 -0400

    quicksave

---
+++ index.js
@@ -0,0 +1,58 @@
+/*
+Copyright 2019 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it. If you have received this file from a source other than Adobe,
+then your use, modification, or distribution of it requires the prior
+written permission of Adobe.
+*/
+
+/* Control the default view mode */
+const viewerConfig = {
+    /* Allowed possible values are "FIT_PAGE", "FIT_WIDTH", "TWO_COLUMN", "TWO_COLUMN_FIT_PAGE" or "". */
+    defaultViewMode: "",
+};
+
+/* Wait for Adobe Acrobat Services PDF Embed API to be ready */
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+    /* Initialize the AdobeDC View object */
+    var adobeDCView = new AdobeDC.View({
+        /* Pass your registered client id */
+        clientId: "cbf93b9f52154aa78547446e15db69aa",
+        /* Pass the div id in which PDF should be rendered */
+        divId: "adobe-dc-view",
+    });
+
+    /* Invoke the file preview API on Adobe DC View object */
+    adobeDCView.previewFile({
+        /* Pass information on how to access the file */
+        content: {
+            /* Location of file where it is hosted */
+            location: {
+                url: "https://raw.githubusercontent.com/natetheadequate/resumes/HEAD/White_Nathaniel_Resume.pdf",
+                // headers: [
+                //     {
+                //         key: "Content-Security-Policy",
+                //         value: "unsafe-inline"
+                //     }
+                // ]
+                /*
+                If the file URL requires some additional headers, then it can be passed as follows:-
+                headers: [
+                    {
+                        key: "<HEADER_KEY>",
+                        value: "<HEADER_VALUE>",
+                    }
+                ]
+                */
+            },
+        },
+        /* Pass meta data of file */
+        metaData: {
+            /* file name */
+            fileName: "Nathaniel White Resume.pdf"
+        }
+    }, viewerConfig);
+});
