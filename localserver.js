538c3d466fda66345678ca6cbb15976e13be6afe
natetheadequate <natewhite345@gmail.com>
2024-08-27 18:43:36 -0400

    last modified is automatic

--- localserver.js
+++ localserver.js
@@ -5,10 +5,19 @@ const PORT = 3000;
 
 // Create the HTTP server
 const server = http.createServer((req, res) => {
+    let url = req.url
+    if(url === "/"){
+        url = "/index.html"
+    }
+    let contentType = (()=>{switch(true) {
+        case url.endsWith(".pdf"): return "application/pdf"
+        case url.endsWith(".html"): return "text/html"
+        default: return "text/plain"
+    }})()
     try{
-        pdfContents = fs.readFileSync(__dirname+req.url)
-        res.writeHead(200, { 'Content-Type': 'application/pdf' , "access-control-allow-origin": "*"})
-        res.end(pdfContents)
+        pdfContents = fs.readFileSync(__dirname+url)
+        res.writeHead(200, { 'Content-Type': contentType , "access-control-allow-origin": "*"})
+        res.end(pdfContents) 
     } catch(e) {
         console.error(e)
         res.writeHead(404, { 'Content-Type': 'text/plain' });
