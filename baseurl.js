5b908b3bc04fd7a660742097559c483061415218
natetheadequate <natewhite345@gmail.com>
2024-09-19 19:43:48 -0400

    redesigned

--- baseurl.js
+++
@@ -1,2 +0,0 @@
-BASE_URL = "https://natetheadequate.github.io/"
-// BASE_URL = "http://localhost:3000/"
--- index.js
+++ index.js
@@ -12,21 +12,22 @@ written permission of Adobe.
 /* Control the default view mode */
 const viewerConfig = {
     /* Allowed possible values are "FIT_PAGE", "FIT_WIDTH", "TWO_COLUMN", "TWO_COLUMN_FIT_PAGE" or "". */
-    defaultViewMode: "FIT_WIDTH",
-    showBookmarks: false,
+    showDownloadPDF: true,
+    focusOnRendering: false,
+    includePDFAnnotations: true,
     showThumbnails: false,
-    showZoomControl: true,
-    showAnnotationTools:false,
-    showFullScreen: true,
-    //embedMode: "SIZED_CONTAINER"
+    showAnnotationTools: false,
+    defaultViewMode: "FIT_WIDTH",
+
 };
 
-function setupPDF(id,link,filename,height){
+function showPdf(filename) {
+    /* Initialize the AdobeDC View object */
     var adobeDCView = new AdobeDC.View({
         /* Pass your registered client id */
-        clientId: "cbf93b9f52154aa78547446e15db69aa",
+        clientId: window.location.hostname === "localhost" ? "ae205f81948145e0a35fd86a2d6891b9" : "cbf93b9f52154aa78547446e15db69aa",
         /* Pass the div id in which PDF should be rendered */
-        divId: id,
+        divId: "adobe-dc-view",
     });
 
     /* Invoke the file preview API on Adobe DC View object */
@@ -35,16 +36,7 @@ function setupPDF(id,link,filename,height){
         content: {
             /* Location of file where it is hosted */
             location: {
-                url: link,
-                // headers: [
-                //     {
-                //         key: "Content-Security-Policy",
-                //         value: "style-src 'self' 'unsafe-inline'"
-                //     },{
-                //         key: "origin",
-                //         value: "natetheadequate.github.io"
-                //     }
-                // ]
+                url: "/" + filename,
                 /*
                 If the file URL requires some additional headers, then it can be passed as follows:-
                 headers: [
@@ -59,22 +51,25 @@ function setupPDF(id,link,filename,height){
         /* Pass meta data of file */
         metaData: {
             /* file name */
-            fileName: filename+".pdf"
+            fileName: filename
         }
-    }, viewerConfig);
-    setTimeout(()=>{
-        document.getElementById(id).style.height = height
-        console.log("setting height")
-    },2000)
+    }, {...viewerConfig,embedMode:+window.innerWidth < 800 ? "IN_LINE": ""});
 }
 
 /* Wait for Adobe Acrobat Services PDF Embed API to be ready */
 document.addEventListener("adobe_dc_view_sdk.ready", function () {
-    setupPDF("resume-div", BASE_URL+"White_Nathaniel_Resume.pdf","White_Nathaniel_Resume","100vw")
-    setupPDF("transcript-div",BASE_URL+"Academic_Transcript.pdf","White_Nathaniel_Transcript","600vw")
-    setTimeout(()=>{
-        document.querySelector(":root").style.setProperty('--pdf-display',"visible");
-        document.getElementById("loading").style.setProperty("display","none")
-        scrollTo(0,0);console.log("making visibale")},3000)
+    const resTab = document.getElementById("resume-tab");
+    const tranTab = document.getElementById("transcript-tab");
+    document.getElementById("resume-btn").addEventListener("click", () => {
+        resTab.classList.add("active-tab");
+        tranTab.classList.remove("active-tab");
+        showPdf("White_Nathaniel_Resume.pdf");
+    })
+    document.getElementById("transcript-btn").addEventListener("click", () => {
+        tranTab.classList.add("active-tab");
+        resTab.classList.remove("active-tab");
+        showPdf("White_Nathaniel_Transcript.pdf");
+    })
+    showPdf("White_Nathaniel_Resume.pdf");
+    document.getElementById("resume-tab").classList.add("active-tab");
 });
-document.addEventListener("adobe_dc_view_sdk")
